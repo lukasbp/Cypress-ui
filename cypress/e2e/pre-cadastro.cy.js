1c52b765625c9a2217ca306fca2ab6a64e1732d4
euae2 <darksleepo@gmail.com>
2024-07-07 18:34:09 -0300

    pre-cadastro

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-cy.faker = require('faker');
+var faker = require('faker');
 
 describe('Funcionalidade pré cadastro', () => {
     
@@ -8,13 +8,18 @@ describe('Funcionalidade pré cadastro', () => {
     });
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(cy.faker.internet.email())
+        
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+        
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('!teste@teste$')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(cy.faker.name.firstName())
-        cy.get('#account_last_name').type(cy.faker.name.lastName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
